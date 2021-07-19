bf7e85b13df4271ef2cce99f28d286e533a5610b
naveenbanjara <19622152+naveenbanjara@users.noreply.github.com>
2021-07-19 23:05:00 +0530

    commit

--- Controllers/ProjectsController.cs
+++ Controllers/ProjectsController.cs
@@ -20,8 +20,35 @@ namespace Project_Management.Controllers
         }
 
         // GET: Projects
-        public async Task<IActionResult> Index()
+        public async Task<IActionResult> Index(string sortOrder, string searchString)
         {
+            ViewData["NameSortParm"] = String.IsNullOrEmpty(sortOrder) ? "name_desc" : "";
+            ViewData["DateSortParm"] = sortOrder == "Date" ? "date_desc" : "Date";
+            ViewData["CurrentFilter"] = searchString;
+
+            var projects = from p in _context.Projects
+                           select p;
+
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                projects = projects.Where(p => p.Name.Contains(searchString));
+            }
+
+            switch (sortOrder)
+            {
+                case "name_desc":
+                    projects = projects.OrderByDescending(p => p.Name);
+                    break;
+                case "Date":
+                    projects = projects.OrderBy(p => p.StartingDate);
+                    break;
+                case "date_desc":
+                    projects = projects.OrderByDescending(p => p.StartingDate);
+                    break;
+                default:
+                    projects = projects.OrderBy(p => p.Name);
+                    break;
+            }
             return View(await _context.Projects.ToListAsync());
         }
 
@@ -56,14 +83,25 @@ namespace Project_Management.Controllers
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("ID,Name,StartingDate,Deadline,EndDate,Status")] Project project)
+        public async Task<IActionResult> Create([Bind("Name,StartingDate,Deadline,EndDate,Status")] Project project)
         {
-            if (ModelState.IsValid)
+            try
             {
-                _context.Add(project);
-                await _context.SaveChangesAsync();
-                return RedirectToAction(nameof(Index));
+                if (ModelState.IsValid)
+                {
+                    _context.Add(project);
+                    await _context.SaveChangesAsync();
+                    return RedirectToAction(nameof(Index));
+                }
             }
+            catch (DbUpdateException)
+            {
+
+                ModelState.AddModelError("", "Unable to save changes. " +
+            "Try again, and if the problem persists " +
+            "see your system administrator.");
+            }
+            
             return View(project);
         }
 
@@ -88,12 +126,14 @@ namespace Project_Management.Controllers
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("ID,Name,StartingDate,Deadline,EndDate,Status")] Project project)
+        public async Task<IActionResult> Edit(int id, [Bind("Name,StartingDate,Deadline,EndDate,Status")] Project project)
         {
             if (id != project.ID)
             {
                 return NotFound();
             }
+
+            //var projectToUpdate = await _context.Projects.FirstOrDefaultAsync(p => p.ID == id);
 
             if (ModelState.IsValid)
             {
@@ -119,7 +159,7 @@ namespace Project_Management.Controllers
         }
 
         // GET: Projects/Delete/5
-        public async Task<IActionResult> Delete(int? id)
+        public async Task<IActionResult> Delete(int? id,bool? saveChangesError=false)
         {
             if (id == null)
             {
@@ -127,10 +167,18 @@ namespace Project_Management.Controllers
             }
 
             var project = await _context.Projects
+                .AsNoTracking()
                 .FirstOrDefaultAsync(m => m.ID == id);
             if (project == null)
             {
                 return NotFound();
+            }
+
+            if (saveChangesError.GetValueOrDefault())
+            {
+                ViewData["ErrorMessage"] =
+                    "Delete failed. Try again, and if the problem persists " +
+                    "see your system administrator.";
             }
 
             return View(project);
@@ -142,9 +190,22 @@ namespace Project_Management.Controllers
         public async Task<IActionResult> DeleteConfirmed(int id)
         {
             var project = await _context.Projects.FindAsync(id);
-            _context.Projects.Remove(project);
-            await _context.SaveChangesAsync();
-            return RedirectToAction(nameof(Index));
+            if (project==null)
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            try
+            {
+                _context.Projects.Remove(project);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            catch (DbUpdateException)
+            {
+
+                return RedirectToAction(nameof(Delete), new { id = id, saveChangesError = true });
+            }
+            
         }
 
         private bool ProjectExists(int id)
